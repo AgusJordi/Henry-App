1655e34895b95916327295203bdea33d72259d7e
Alberto M. Popelka <63745642+apopelka87@users.noreply.github.com>
2020-09-11 18:03:28 -0300

    ruta para buscar por iLike en nombre y apellido

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,10 +4,12 @@ const router = Router();
 /* const productRouter = require('./product.js'); */
 const userRouter = require("./user");
 const checkpointRouter = require("./checkpoint");
+const searchRouter = require("./search");
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 
 router.use("/users", userRouter);
 router.use("/checkpoints", checkpointRouter);
+router.use("/search", searchRouter);
 
 module.exports = router;
