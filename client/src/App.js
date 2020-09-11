9693f5800a0e64bf88c0b44773eaa018590030b6
JeremiasKoch <jeremiaskoch.92@gmail.com>
2020-09-10 21:05:37 -0300

    add register in login

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import Home from './components/home';
- 
-import login from './components/Login.jsx';
- 
+import login from './components/Login';
 import Navbar from './components/Navbar.jsx';
- 
 import {Switch, Route, Link, Redirect} from 'react-router-dom';
 import axios from 'axios';
 import Sidebar from './components/sidebar';
 
 function App() {
+
   return (
   	<div>
   	<Route
-          path='/'
-          component={Navbar}
-        />
+      path='/'
+      component={Navbar}
+    />
     <Route
-          path='/'
-          component={Sidebar}
-      />   
+      path='/'
+      component={Sidebar}
+    />
   	<Switch>
       <Route path='/home' component={Home} />    
       <Route path="/login" component={login} /> 
     </Switch>
-    
-    
     </div>
   );
 }
