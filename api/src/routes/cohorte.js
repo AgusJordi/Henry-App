0e0084eb40d627bcc66f11fe79240904e80255c9
agus rojas <agusrojasrp@gmail.com>
2020-09-11 14:54:39 -0300

    ruta-cohorte

---
+++ api/src/routes/cohorte.js
@@ -0,0 +1,58 @@
+const server = require("express").Router();
+
+const { Cohorte } = require("../db.js");
+
+server.get("/", (req, res, next) => {
+    Cohorte.findAll()
+    .then((cohortes) => {
+        if (cohortes.length === 0) {
+            return res.send ({ message: "No se han creado cohortes"});
+        }
+        return res.send(cohortes);
+    })
+    .catch((error) => next(error));
+});
+
+server.post("/", (req, res, next) => {
+    const { name, description } = req.body;
+    Cohorte.create({ name, description })
+    .then((cohorte) => {
+        res.send(cohorte);
+    })
+    .catch((error) => next(error));
+});
+
+server.delete("/:name", (req, res, next) => {
+    const checkName = req.params.name;
+    Cohorte.destroy({ where: {heckName: name} })
+    .then((num) => {
+        if (num > 0) {
+          return res.send({ message: `Se elimino el Cohorte : ${checkName}` });
+        }
+        return res.send({
+          message: `No se pudo eliminar el Cohorte: ${checkName}`,
+        });
+      })
+      .catch((err) => next(err));
+});
+
+server.put("/:name", async (req, res, next) => {
+    const nameCohorte = req.params.name;
+    const { name, description } = req.body;
+  
+    try {
+      const cohorte = await Cohorte.findOne({ where: { name: nameCohorte } });
+      if (!cohorte) {
+        return res.send({
+          message: `No se encontro el Cohorte: ${nameCohorte}`,
+        });
+      }
+      const cohorteUpdate = await cohorte.update({
+        name: name,
+        description: description,
+      });
+      return res.send(cohorteUpdate);
+    } catch (error) {
+      next(error);
+    }
+  });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,10 +4,12 @@ const router = Router();
 /* const productRouter = require('./product.js'); */
 const userRouter = require("./user");
 const checkpointRouter = require("./checkpoint");
+const cohorteRouter = ("./cohorte");
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 
 router.use("/users", userRouter);
 router.use("/checkpoints", checkpointRouter);
+router.use("/cohorte", cohorteRouter);
 
 module.exports = router;
