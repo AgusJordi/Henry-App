404f34fc57d3ba443a280618f8c87c271d5d8464
Vicky <vlcabrera92@hotmail.com>
2020-09-22 00:30:00 -0300

    cada nombre abre el profile

--- client/src/components/modalUsers.jsx
+++ client/src/components/modalUsers.jsx
@@ -2,6 +2,7 @@ import martin from "../images/martinborchardt.png";
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import Profile from "./Profile";
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -43,18 +44,24 @@ const useStyles = makeStyles((theme) => ({
     padding: "5px",
     outline: "none",
     width: "-webkit-fill-available",
-    marginBottom: "10px"
+    marginBottom: "10px",
+    "&:focus": {
+      outline:"auto"
+    },
   },
   divs:{
     display: "flex",
     margin: "10px"
   },
   users:{
-      overflow: "auto",
-      maxHeight: "400px"
+    overflow: "auto",
+    maxHeight: "360px",
+  },
+  h3:{
+    "&:hover": {
+      cursor: "pointer",
+    },
   }
-
-
 }));
 
 export default function ModalUsers(props) {
@@ -62,51 +69,61 @@ export default function ModalUsers(props) {
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(true);
-
-
   const [miembros, setMiembros] = React.useState(users);
   const [filtrados, setFiltrados] = React.useState(users);
   const [input, setInput] = React.useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [modalUser, setModalUser] = useState([]);
 
   // const handleOpen = () => {
   //   setOpen(true);
   // };
-
+  const handleOpenModal = (value) => {
+    setOpenModal(true);
+    setModalUser(value);
+  };
+  
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+  
   const handleClose = () => {
     setOpen(false);
     props.show(false)//pasarle desde el componente padre una funcion show por props
   };
 
 // setea el input 
-    const onChange = (event) => {
-        const inputValue = event.target.value.toLowerCase();
-        setInput(inputValue);
-    } 
+  const onChange = (event) => {
+      const inputValue = event.target.value.toLowerCase();
+      setInput(inputValue);
+  } 
+
 
 //filtra cada vez que cambia el input.value
-    useEffect(() => {
-      filterList();
-    },[input]);
+  useEffect(() => {
+    filterList();
+  },[input]);
 
-    const filterList = ()=> {
-      let users = miembros;
-      let q = input;
-      
-      users = users.filter(function(user) {
-        return user.name.toLowerCase().indexOf(q) != -1 || user.lastName.toLowerCase().indexOf(q) != -1; // returns true or false
-      });
-      setFiltrados (users)
-    }
+  const filterList = ()=> {
+    let users = miembros;
+    let q = input;
+    
+    users = users.filter(function(user) {
+      return user.name.toLowerCase().indexOf(q) != -1 || user.lastName.toLowerCase().indexOf(q) != -1; // returns true or false
+    });
+    setFiltrados (users)
+  }
 
-    const listaFiltrados=  filtrados.map(user=> {
-        return (<div className={classes.divs}>
-            <img className={classes.img} src={martin}/>
-            <h3> {user.name+" "+user.lastName}</h3>
-            </div>)
-    })
+  const listaFiltrados=  filtrados.map(user=> {
+      return (<div className={classes.divs}>
+          <img className={classes.img} src={martin}/>
+          <h3 className={classes.h3} onClick={() => handleOpenModal(user)}> {user.name+" "+user.lastName}</h3>
+          </div>)
+  })
 
   return (
     <div className={classes.father}>
+      <Profile user={modalUser} state={openModal} close={handleCloseModal} />
         <Modal
             open={open}
             onClose={handleClose}
@@ -132,6 +149,7 @@ export default function ModalUsers(props) {
         </div>
     </div>
     </Modal>
+    
     </div>
   );
 }
