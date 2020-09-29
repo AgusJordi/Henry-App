d4c5a8a2dd6cf329c1e23afa9c2b03f3307c408f
Ceci Hansen <ceci.hansen@gmail.com>
2020-09-29 12:07:37 -0300

    ruta put grupo arreglada

--- api/src/routes/group.js
+++ api/src/routes/group.js
@@ -84,20 +84,6 @@ server.put("/:id", async (req, res, next) => {
   console.log(req.body)
   const { PM1Id, PM2Id } = req.body;
 
-
-  try {
-    const group = await User.findOne({ where: { id: id } });
-    if (!group) {
-      return res.send({
-        message: ``,
-      });
-    }
-    const studentUpdate = await student.update({
-      cohorteId: cohorteId,
-      description: description,
-    });
-    return res.send(groupUpdate);
-
   Group.findOne({
     where: {
       id: idGroup
@@ -128,28 +114,28 @@ server.put("/:id", async (req, res, next) => {
 server.post('/add', async (req, res, next) => {
   const { cohorteId, cantidadGrupos, usuariosHabilitados } = req.body;
 
-  try{
+  try {
     const students = usuariosHabilitados; //array de estudiantes habilitados de ese cohorte
     const idGroup = []; // id de grupos
     const cantidadStudents = students.length; //cantidad estudiantes
-  
-    for (let i = 0; i < cantidadGrupos; i++){ 
+
+    for (let i = 0; i < cantidadGrupos; i++) {
       idGroup.push((await Group.create({ cohorteId })).dataValues.id)
     }
-  
-    for(let i = 0; i < idGroup.length; i++){
-      for(let j = 0; j < Math.floor(cantidadStudents/idGroup.length); j++){ //cuantos estudiantes hay x grupo
+
+    for (let i = 0; i < idGroup.length; i++) {
+      for (let j = 0; j < Math.floor(cantidadStudents / idGroup.length); j++) { //cuantos estudiantes hay x grupo
         const al = await Student.findByPk((students.shift()).id)
-        await al.update({groupId: idGroup[i]})
+        await al.update({ groupId: idGroup[i] })
       }
     }
 
-    if( students ){ //Caso en que la division tenga resto => Se mete los que sobran en el 1er grupo
-      for ( let i= 0; i<students.length; i++){
-        console.log("ultimo",students)
-        const st= await Student.findByPk((students.shift()).id)
-        console.log( "st", st)
-        await st.update({groupId: idGroup[0]})
+    if (students) { //Caso en que la division tenga resto => Se mete los que sobran en el 1er grupo
+      for (let i = 0; i < students.length; i++) {
+        console.log("ultimo", students)
+        const st = await Student.findByPk((students.shift()).id)
+        console.log("st", st)
+        await st.update({ groupId: idGroup[0] })
       }
     }
 
@@ -163,7 +149,7 @@ server.post('/add', async (req, res, next) => {
 });
 
 
-module.exports = server; 
+module.exports = server;
 
 
 
